173db8f64531b8d49732865965a4d280fe44c213
Hebilicious <xsh4k3@gmail.com>
2023-07-04 05:49:44 +0700

    feat: add libsql to index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./types";
 
 export const connectors = {
   sqlite: "db0/connectors/better-sqlite3",
+  libsql: "db0/connectors/libsql",
   postgresql: "db0/connectors/postgresql",
   "cloudflare-d1": "db0/connectors/cloudflare-d1",
 } as const;
