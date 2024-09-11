49f9f9585dde5de7377566421239875cb973eb82
hareland <packagist@proton.me>
2024-09-11 10:07:19 +0200

    (chore): Rename default postgresql.ts export to match postgresqlConnector for typehinted import.

--- src/connectors/postgresql.ts
+++ src/connectors/postgresql.ts
@@ -4,7 +4,7 @@ import type { Connector, Statement } from "../types";
 
 export type ConnectorOptions = { url: string } | ClientConfig;
 
-export default function sqliteConnector(opts: ConnectorOptions) {
+export default function postgresqlConnector(opts: ConnectorOptions) {
   let _client: undefined | Client | Promise<Client>;
   function getClient() {
     if (_client) {
