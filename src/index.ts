ecb54a8275dbb3d86a6f096438ef6d358055ab42
Max <maximogarciamtnez@gmail.com>
2024-10-09 20:57:22 +0200

    docs: added jsdocs to exported functions and types (#89)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export * from "./database";
 export * from "./types";
 
+/**
+ * A mapping of available database connector identifiers to their module paths.
+ * This constant facilitates the use of different database connectors by providing easy access to the
+ * by providing easy access to the connector's specific import paths.
+ */
 export const connectors = {
   sqlite: "db0/connectors/better-sqlite3",
   postgresql: "db0/connectors/postgresql",
@@ -13,4 +18,8 @@ export const connectors = {
   "bun-sqlite": "db0/connectors/bun-sqlite",
 } as const;
 
+/**
+ * Type alias for the keys of the {@link connectors} map.
+ * Represents the names of available database connectors.
+ */
 export type ConnectorName = keyof typeof connectors;
