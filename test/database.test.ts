f002d50d1f37468bec5c257a59539f5144b5458d
Jonathan Ginn <jonathan@ginn.work>
2024-12-24 02:48:45 -0500

    Removed tests forcing "returning" in names in database.test.ts

--- test/database.test.ts
+++ test/database.test.ts
@@ -2,40 +2,21 @@ import { describe, expect, it } from "vitest";
 import { createDatabase } from "../src/database";
 import sqlite from "../src/connectors/better-sqlite3";
 
-const templates = {
-  default: `
-    CREATE TABLE users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      age INTEGER NOT NULL
-    )
-  `,
-  column: `
-    CREATE TABLE users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      age INTEGER NOT NULL,
-      "returning" INTEGER DEFAULT NULL
-    )
-  `,
-  table: `
-    CREATE TABLE "returning" (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      age INTEGER NOT NULL
-    )
-  `,
-};
-
-const setup = async (template: string) => {
+const setup = async () => {
   const db = createDatabase(sqlite({ name: ":memory:" }));
-  await db.sql`{${template}}`;
+  await db.sql`
+    CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      age INTEGER NOT NULL
+    )
+  `;
   return db;
 };
 
 describe("Database RETURNING clause", () => {
   it("Returns success when not using RETURNING", async () => {
-    const db = await setup(templates.default);
+    const db = await setup();
     const creates =
       await db.sql`INSERT INTO users (name, age) VALUES (${"Alice"}, ${25})`;
     const updates = await db.sql`UPDATE users SET age = ${26} WHERE id = ${1}`;
@@ -47,7 +28,7 @@ describe("Database RETURNING clause", () => {
   });
 
   it("Returns rows when using RETURNING", async () => {
-    const db = await setup(templates.default);
+    const db = await setup();
     const creates =
       await db.sql`INSERT INTO users (name, age) VALUES (${"Alice"}, ${25}) RETURNING *`;
     const updates =
@@ -57,31 +38,5 @@ describe("Database RETURNING clause", () => {
     expect(creates).toMatchObject({ rows: [{ name: "Alice", age: 25 }] });
     expect(updates).toMatchObject({ rows: [{ name: "Alice", age: 26 }] });
     expect(deletes).toMatchObject({ rows: [{ name: "Alice", age: 26 }] });
-  });
-
-  describe("Ignores RETURNING used as non-keyword", () => {
-    it('Ignores value "returning"', async () => {
-      const db = await setup(templates.default);
-      const value =
-        await db.sql`INSERT INTO users (name, age) VALUES (${"RETURNING"}, ${25})`;
-
-      expect(value).toMatchObject({ success: true });
-    });
-
-    it('Ignores column "returning"', async () => {
-      const db = await setup(templates.column);
-      const column =
-        await db.sql`INSERT INTO users (name, age, "returning") VALUES (${"Alice"}, ${25}, ${50})`;
-
-      expect(column).toMatchObject({ success: true });
-    });
-
-    it('Ignores table "returning"', async () => {
-      const db = await setup(templates.table);
-      const table =
-        await db.sql`INSERT INTO "returning" (name, age) VALUES (${"Alice"}, ${25})`;
-
-      expect(table).toMatchObject({ success: true });
-    });
   });
 });
