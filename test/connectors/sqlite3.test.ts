d690c33c3b65814b10bd7bcbea8764eeabcbb979
Farnabaz <farnabaz@gmail.com>
2025-04-16 15:26:13 +0200

    test: add callback test

--- test/connectors/sqlite3.test.ts
+++ test/connectors/sqlite3.test.ts
@@ -1,4 +1,4 @@
-import { describe } from "vitest";
+import { describe, test, expect } from "vitest";
 import connector from "../../src/connectors/sqlite3";
 import { testConnector } from "./_tests";
 
@@ -9,4 +9,33 @@ describe("connectors: sqlite3", () => {
       name: ":memory:",
     }),
   });
+
+  test('Sqlite3 prepare callback', async () => {
+    const db = connector({
+      name: ":memory:",
+    })
+
+    // Node Sqlite3 prepare callback only calls prepare callback on error
+    const instance = await db.getInstance();
+    const result = await new Promise((resolve) => {
+      const calls = [] as string[]
+      instance.prepare("SELECT * FROM table_1", () => {
+        calls.push('prepare callback')
+        setTimeout(() => {
+          resolve(calls)
+        }, 1000)
+      }).all(() => {
+        calls.push('all callback')
+        setTimeout(() => {
+          resolve(calls)
+        }, 1000)
+      })
+    })
+    // Only prepare callback is called because the table doesn't exist
+    expect(result).toStrictEqual(['prepare callback'])
+
+    // Sqlite3 Connection solves this by forwarding the error to final callback
+    const res = await db.prepare("SELECT * FROM table_1").all().catch(() => "REJECTED");
+    expect(res).toBe("REJECTED");
+  })
 });
